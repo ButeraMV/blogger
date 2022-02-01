42d6d8758f704526be8f0ba59eeb98fac42f507b
Michael Butera <buteramv@gmail.com>
2022-01-31 20:39:17 -0500

    Add article edit

---
+++ spec/features/user_edits_an_article_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'user edits an article' do
+    describe 'they link from the articles index' do
+        describe 'they fill in a title and body' do
+            it 'creates a new article' do
+                article = Article.create!(title: 'Old Title', body: 'Old Body')
+
+                visit article_path(article)
+                click_link 'Edit'
+
+                expect(current_path).to eq(edit_article_path(article))
+
+                fill_in 'article[title]', with: 'New Title!'
+                fill_in 'article[body]', with: 'New Body!'
+                click_on 'Update Article'
+
+                expect(page).to have_content('New Title!')
+                expect(page).to have_content('New Body!')
+            end
+        end
+    end
+end
