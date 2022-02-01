c506d67ef62303a0a689fbd7c602d6260f09b3cb
Michael Butera <buteramv@gmail.com>
2022-01-31 20:56:59 -0500

    Replace tab indentation with spaces

--- spec/features/user_edits_an_article_spec.rb
+++ spec/features/user_edits_an_article_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 describe 'user edits an article' do
-    describe 'they link from the articles index' do
-        describe 'they fill in a title and body' do
-            it 'creates a new article' do
-                article = Article.create!(title: 'Old Title', body: 'Old Body')
+  describe 'they link from the articles index' do
+    describe 'they fill in a title and body' do
+      it 'creates a new article' do
+        article = Article.create!(title: 'Old Title', body: 'Old Body')
 
-                visit article_path(article)
-                click_link 'Edit'
+        visit article_path(article)
+        click_link 'Edit'
 
-                expect(current_path).to eq(edit_article_path(article))
+        expect(current_path).to eq(edit_article_path(article))
 
-                fill_in 'article[title]', with: 'New Title!'
-                fill_in 'article[body]', with: 'New Body!'
-                click_on 'Update Article'
+        fill_in 'article[title]', with: 'New Title!'
+        fill_in 'article[body]', with: 'New Body!'
+        click_on 'Update Article'
 
-                expect(page).to have_content('New Title!')
-                expect(page).to have_content('New Body!')
-                expect(page).to have_content("Article 'New Title!' Updated!")
-            end
-        end
+        expect(page).to have_content('New Title!')
+        expect(page).to have_content('New Body!')
+        expect(page).to have_content("Article 'New Title!' Updated!")
+      end
     end
+  end
 end
--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 describe 'user sees all articles' do
-    describe 'they visit /articles' do
-        it 'displays all articles' do
-            article_1 = Article.create!(title: 'Title 1', body: 'Body 1')
-            article_2 = Article.create!(title: 'Title 2', body: 'Body 2')
+  describe 'they visit /articles' do
+    it 'displays all articles' do
+      article_1 = Article.create!(title: 'Title 1', body: 'Body 1')
+      article_2 = Article.create!(title: 'Title 2', body: 'Body 2')
 
-            visit '/articles'
+      visit '/articles'
 
-            expect(page).to have_link(article_1.title)
-            expect(page).to have_link(article_2.title)
-        end
-
-        it 'displays a link to create a new article' do
-            visit '/articles'
-
-            expect(page).to have_link("Create a New Article")
-        end
+      expect(page).to have_link(article_1.title)
+      expect(page).to have_link(article_2.title)
     end
+
+    it 'displays a link to create a new article' do
+      visit '/articles'
+
+      expect(page).to have_link("Create a New Article")
+    end
+  end
 end
